6a215ada6a88a3ba56c85aec61ba15a6f1016f3a
JIPHEENS <JIPHEENS@gmail.com>
2023-07-08 23:52:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import LoginPage from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RegisterPage from './components/register';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Fetchdata from './components/ListOfRfq'; 
+import ApplicationForm from './components/ApplicationForm';
+// import Admindashboard from './Admindashboard';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+<div className='App'>
+     <Router>
+    <Routes>
+    {/* <Route path="/" element={<LoginPage />} />  
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/register" element={<RegisterPage  />} />
+    
+     <Route path='/' element={<Fetchdata />} />*/}
+     <Route path='/' element={<ApplicationForm />} />
+
+
+       </Routes>
+    </Router>  
     </div>
-  );
+
+
+
+
+
+    );
 }
 
 export default App;
